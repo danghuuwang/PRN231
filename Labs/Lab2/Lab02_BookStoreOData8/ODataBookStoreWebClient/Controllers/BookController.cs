cdcebafd814135f52a66f477a166cf1bac3feea6
Dang Huu Quang <handsomeoptics52@gmail.com>
2023-03-05 12:42:11 +0700

    PRN231 - Lab 2 (Finish)

---
+++ Labs/Lab2/Lab02_BookStoreOData8/ODataBookStoreWebClient/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Mvc;
+using ODataBookStore.Models;
+
+namespace ODataBookStoreWebClient.Controllers;
+
+public class BookController : Controller
+{
+    private readonly HttpClient client;
+
+    private readonly string ProductApiUrl = "";
+
+    public BookController()
+    {
+        client = new HttpClient();
+        var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+        client.DefaultRequestHeaders.Accept.Add(contentType);
+        ProductApiUrl = "http://localhost:7061/odata/Books";
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var response = await client.GetAsync(ProductApiUrl);
+        var strData = await response.Content.ReadAsStringAsync();
+
+        dynamic temp = JsonNode.Parse(strData);
+        var lstBooks = temp.value;
+
+        var items = ((JsonArray)temp.value).Select(x => new Book
+        {
+            Id = (int)x["Id"],
+            ISBN = (string)x["ISBN"],
+            Title = (string)x["Title"],
+            Author = (string)x["Author"],
+            Price = (decimal)x["Price"]
+        }).ToList();
+
+        return View(items);
+    }
+}
---
+++ Labs/Lab2/Lab02_BookStoreOData8/ODataBookStoreWebClient/Controllers/PressController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Mvc;
+using ODataBookStore.Models;
+
+namespace ODataBookStoreWebClient.Controllers;
+
+public class PressController : Controller
+{
+    private readonly HttpClient client;
+    private readonly string ProductApiUrl = "";
+
+    public PressController()
+    {
+        client = new HttpClient();
+        var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+        client.DefaultRequestHeaders.Accept.Add(contentType);
+        ProductApiUrl = "http://localhost:7010/odata/Presses";
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var response = await client.GetAsync(ProductApiUrl);
+        var strData = await response.Content.ReadAsStringAsync();
+        dynamic temp = JsonNode.Parse(strData);
+
+        var lst = temp.value;
+        var items = ((JsonArray)temp.value).Select(x => new Press
+        {
+            Id = (int)x["Id"],
+            Name = (string)x["Name"]
+        }).ToList();
+        return View(items);
+    }
+}
